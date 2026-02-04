602ea91d4a0ad074de67b7937afe3f9dba5ce2c9
Cassidy Coombs <cassidyrose56@gmail.com>
2026-02-03 19:17:05 -0500

    next.js app created, data added

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen p-8 bg-white">
+      <h1 className="font-bold text-xl text-black">Shop Search</h1>
+    </main>
+  );
+}
